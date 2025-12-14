b47fe6164f85c85e5c8a10ac2e58248b2d628883
decasagallo <diegoeduardo0507@gmail.com>
2025-12-13 20:35:57 -0500

    Eliminar dotenv y usar variables de entorno para Render bien

--- src/main/java/ec/com/ecuamag/InventarioDigital/InventarioDigitalApplication.java
+++ src/main/java/ec/com/ecuamag/InventarioDigital/InventarioDigitalApplication.java
@@ -4,7 +4,6 @@ import ec.com.ecuamag.InventarioDigital.service.serviceClise.CsvImportClienteCli
 import ec.com.ecuamag.InventarioDigital.service.serviceClise.CsvImportCliseService;
 import ec.com.ecuamag.InventarioDigital.service.servicePlaca.CsvImportPlacaService;
 import ec.com.ecuamag.InventarioDigital.service.serviceTroquel.CsvImportTroquelService;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,49 +13,26 @@ import org.springframework.context.annotation.Bean;
 public class InventarioDigitalApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(InventarioDigitalApplication.class, args);
 	}
 
 	@Bean
-	CommandLineRunner importarTroqueles(CsvImportTroquelService csvImportTroquelService) {
-		return args -> {
-			csvImportTroquelService.importarDatosDesdeCsv(); // Eliminado el parámetro filePath
-			System.out.println("📥 Importación de troqueles desde CSV completada.");
-		};
-	}
-
-
-	@Bean
-	CommandLineRunner importarClises(CsvImportTroquelService csvImportTroquelService,
-						  CsvImportCliseService csvImportCliseService) {
-		return args -> {
-			csvImportCliseService.importarDatosDesdeCsv(); // 🆕 se ejecuta al iniciar
-			System.out.println("📥 Importación de clises desde csv completada.");
-		};
+	CommandLineRunner importarTroqueles(CsvImportTroquelService service) {
+		return args -> service.importarDatosDesdeCsv();
 	}
 
 	@Bean
-	CommandLineRunner importarClienteClises(
-			CsvImportClienteCliseService service
-	) {
-		return args -> {
-			service.importarDesdeCsv();
-			System.out.println("📥 Importación de clises  desde csv completada.");
-		};
+	CommandLineRunner importarClises(CsvImportCliseService service) {
+		return args -> service.importarDatosDesdeCsv();
+	}
+
+	@Bean
+	CommandLineRunner importarClienteClises(CsvImportClienteCliseService service) {
+		return args -> service.importarDesdeCsv();
 	}
 
 	@Bean
 	CommandLineRunner importarPlacas(CsvImportPlacaService service) {
-		return args -> {
-			service.importarDesdeCsv();
-			System.out.println("📥 Importación de placas desde CSV completada.");
-		};
+		return args -> service.importarDesdeCsv();
 	}
-
-
-
 }
